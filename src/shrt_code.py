7169e01768b955a6748caf5a71c3f42d9a1d6267
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 11:04:30 +0300

    Update shrt_code.py

--- src/shrt_code.py
+++ src/shrt_code.py
@@ -1,44 +1,38 @@
-from requests import get
+from requests import Session
 
 class ShrtCode:
-	def __init__(self) -> None:
-		self.api = "https://api.shrtco.de/v2"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36"
-		}
-	
-	def shorten_url(self, url: str) -> dict:
-		return get(
-			f"{self.api}/shorten?url={url}",
-			headers=self.headers).json()
-	
-	def protect_url(
-			self,
-			url: str,
-			password: str) -> dict:
-		return get(
-			f"{self.api}/shorten?url={url}&password={password}",
-			headers=self.headers).json()
-	
-	def generate_emoji_url(self, url: str) -> dict:
-		return get(
-			f"{self.api}/shorten?emoji&url={url}",
-			headers=self.headers).json()
-			
-	def get_url_information(self, code: str) -> dict:
-		return get(
-			f"{self.api}/info?code={code}",
-			headers=self.headers).json()
-	
-	def get_status(self) -> dict:
-		return get(
-			f"{self.api}/status",
-			headers=self.headers).json()
-	
-	def generate_custom_url(
-			self,
-			url: str,
-			custom_code: str) -> dict:
-		return get(
-			f"{self.api}/shorten?url={url}&custom_code={custom_code}",
-			headers=self.headers).json()
+    def __init__(self) -> None:
+        self.api = "https://api.shrtco.de/v2"
+        self.session = Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36"
+        }
+    
+    def shorten_url(self, url: str) -> dict:
+        return self.session.get(f"{self.api}/shorten?url={url}").json()
+    
+    def protect_url(
+            self,
+            url: str,
+            password: str) -> dict:
+        return self.session.get(
+            f"{self.api}/shorten?url={url}&password={password}").json()
+    
+    def generate_emoji_url(self, url: str) -> dict:
+        return self.session.get(
+            f"{self.api}/shorten?emoji&url={url}").json()
+            
+    def get_url_information(self, code: str) -> dict:
+        return self.session.get(
+            f"{self.api}/info?code={code}").json()
+    
+    def get_status(self) -> dict:
+        return self.session.get(
+            f"{self.api}/status").json()
+    
+    def generate_custom_url(
+            self,
+            url: str,
+            custom_code: str) -> dict:
+        return self.session.get(
+            f"{self.api}/shorten?url={url}&custom_code={custom_code}").json()
